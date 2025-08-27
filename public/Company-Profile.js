bbcc65895b2411a1e3193d5c1fd38b9c79f96520
Eazybel <eazybel27@gmail.com>
2025-08-27 23:21:08 +0300

    update Company-Profile.html and register.html

---
+++ public/Company-Profile.js
@@ -0,0 +1,21 @@
+const messageBox = document.getElementById("message-box");
+const registrationForm = document.getElementById("registration-form");
+// Company Information
+const companyName = document.getElementById("company-name");
+const sector = document.getElementById("sector");
+const address = document.getElementById("address");
+const employees = document.getElementById("employees");
+const logo = document.getElementById("logo");
+const foundationDate = document.getElementById("foundation-date");
+// Primary Contact
+const primaryName = document.getElementById("primary-name");
+const primaryEmail = document.getElementById("primary-email");
+const primaryPhone = document.getElementById("primary-phone");
+// HR Contact
+const hrName = document.getElementById("hr-name");
+const hrEmail = document.getElementById("hr-email");
+const hrPhone = document.getElementById("hr-phone");
+// Admin Account
+const adminEmail = document.getElementById("admin-email");
+const adminPassword = document.getElementById("admin-password");
+const confirmPassword = document.getElementById("confirm-password");
